e4d78eb8c2773992fc015ac5191492dc93d44d91
naru-77 <naru.fumi@icloud.com>
2021-10-01 17:22:08 +0900

    update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,32 @@ import './App.css'
  * 
  * @type {React.FC}
  */
+
 export const App = () => {
+
+  const changeUrl = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const status = data.status;
+      if(status === "success") {
+        const dogUrl = data.message;
+        setDogUrl(dogUrl);
+      }
+    });
+  }
+
+  const [dogUrl, setDogUrl] = React.useState(" images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
   return (
     <div>
-      <header>kooo</header>
-      <p>犬の画像</p>
-      <img src="https://dog.ceo/api/breeds/image/random"></img>
+      <header className="header">DOGアプリ</header>
+      <p　className="description">犬の画像を表示します</p>
+      <img src={dogUrl}></img>
+      <button　onClick={changeUrl}>更新</button>
     </div>
-  )
-}
+
+
+  );
+};
