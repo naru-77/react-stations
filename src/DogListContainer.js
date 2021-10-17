6971cd7676e64826f4298eab51e5f60a0b1df99e
naru-77 <naru.fumi@icloud.com>
2021-10-17 14:07:13 +0900

    station9

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,17 @@
 // DO NOT DELETE
+import React from "react";
+import {useState, useEffect} from 'react'
+
+export function DogListContainer(){
+    const [breeds,setBreeds] = useState([])
+
+    useState(()=>{
+        fetch('https://dog.ceo/api/breeds/list/all')
+        .then(res=>res.json())
+        .then(result => {
+            const breedsResult = result.message
+            setBreeds(breedsResult)
+        })
+        .catch(err => {})
+    },[])
+}
