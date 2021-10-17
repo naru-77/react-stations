2981fa4f5fee0472ce6aaeaf1cafbd3a438f078b
naru-77 <naru.fumi@icloud.com>
2021-10-17 23:46:41 +0900

    station9 クリア

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,7 @@
 // DO NOT DELETE
+import React from "react";
+
+export function BreedsSelect(props){
+    const breeds = props.breeds
+<option>{breeds}</option>
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -14,4 +14,5 @@ export function DogListContainer(props){
         })
         .catch(err => {})
     },[])
+    return null
 }
