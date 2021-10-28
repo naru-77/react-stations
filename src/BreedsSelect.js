812399b47216742b5f37bc0b09442fe97d93e19b
naru-77 <naru.fumi@icloud.com>
2021-10-28 09:14:18 +0900

    station11

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,7 +1,51 @@
 // DO NOT DELETE
-import React from "react";
+import React,{useState,useEffect} from 'react';
+import { DogListContainer } from './DogListContainer';
 
-export function BreedsSelect(props){
-    const breeds = props.breeds
-<option>{breeds}</option>
+export const BreedsSelect = (props) =>{
+    
+    const breeds =props.breeds;
+    // const imageList = props.imageList;
+
+    const listItems = breeds.map((breeds)=>
+        <option key={breeds} value={breeds} text={breeds}>{breeds}</option>
+    );
+
+    // const image = imageList.map((imageList)=>
+    //     <div>
+    //     <img src={imageList}></img>
+    //     </div>
+    // );
+
+    return　(
+        <React.Fragment>
+            <select value={props.value} onChange={props.handleChange}>
+                {listItems}
+            </select>
+            <div class="button_wrapper">
+            <button onClick={props.onButtonClick}>表示する</button>
+            </div>
+            {/* {image} */}
+        </React.Fragment>
+    )
 }
+// import React from "react";
+
+
+// export function BreedsSelect(props){
+//     const breeds = props.breeds
+//     const handleChange = props.handleChange
+//     const selectBreed = props.selectBreed
+
+//     const listItems = breeds.map((breed) => (
+//         <option key={breed} value={breed} text={breed}>
+//             {breed}
+//         </option>
+//       ))
+// return(
+//     <select　onChange={handleChange} selectBreed={selectBreed}>
+//         <option></option>
+//         {listItems}
+//     </select>
+// )
+// }
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,18 +1,76 @@
 // DO NOT DELETE
-import React from "react";
-import {useState, useEffect} from 'react'
+import React,{useState,useEffect} from 'react';
+import { BreedsSelect } from './BreedsSelect';
 
-export function DogListContainer(props){
-    const [breeds,setBreeds] = useState([])
+export const DogListContainer = (props) =>{
 
-    useEffect(()=>{
-        fetch('https://dog.ceo/api/breeds/list/all')
-        .then(res=>res.json())
-        .then(result => {
-            const breedsResult = result.message
-            setBreeds(breedsResult)
-        })
-        .catch(err => {})
-    },[])
-    return null
+    // APIで拾ってきた犬種リスト
+    const [breeds, setBreeds] = useState([]);
+
+    // プルダウンで選択した犬
+    const[selectedBreed,setSelectedBreed]= useState("affenpinscher")
+
+    // //クリックで取得した犬の画像リスト
+    // const[imageList,setImagelist]=useState("")
+
+    //onChanegeで実行する関数
+    const handleChange = (option) =>{
+        setSelectedBreed(option.target.value)
+    }
+
+    // //表示するボタンで実行する関数
+    // const onButtonClick = () =>{
+    //     fetch("https://dog.ceo/api/breed/"+selectedBreed+"/images/random/12")
+    //     .then(res => res.json())
+    //     .then(imageData => setImagelist(imageData.message))
+    // }
+  
+    useEffect (() =>{
+      fetch("https://dog.ceo/api/breeds/list/all")
+      .then(res => res.json())
+      .then(data => setBreeds(data.message))
+    },[]);
+
+    return (
+        <React.Fragment>
+            <BreedsSelect 
+            breeds={Object.keys(breeds)}
+            value={selectedBreed}
+            handleChange={handleChange}
+            // onButtonClick={onButtonClick}
+            // imageList={Object.values(imageList)}
+            />
+        </React.Fragment>
+    );
 }
+// import React from "react";
+// import {useState, useEffect} from 'react'
+// import { BreedsSelect } from './BreedsSelect';
+
+// export function DogListContainer(props){
+//     const [breeds,setBreeds] = useState([])
+//     const [selectBreed, setSelectBreed]=useState('')
+
+//     const handleChange = (event) => {
+//         setSelectBreed(event.target.value)
+//       }
+
+//     useEffect(()=>{
+//         fetch('https://dog.ceo/api/breeds/list/all')
+//         .then(res=>res.json())
+//         .then(result => {
+//             const breedsResult = result.message
+//             setBreeds(breedsResult)
+//         })
+//         .catch(err => {})
+//     },[])
+
+//     return (
+//         <BreedsSelect
+//         breeds = {breeds}
+//         value={selectBreed}
+//         handleChange={handleChange}
+//         selectBreed={selectBreed}
+//         />
+//     )
+// }
